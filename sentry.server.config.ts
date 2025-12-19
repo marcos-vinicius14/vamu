b35b40258fa9306a254b9e291251fb40a09f17f7
marcos <marcos.vinicius.dev3@gmail.com>
2025-12-18 21:16:14 -0300

    .

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,11 @@
-import * as Sentry from "@sentry/nuxt";
- 
-Sentry.init({
-  dsn: "https://f9408e0981c2e91e628fd1cb4238b490@o4510558160945152.ingest.us.sentry.io/4510558175428608",
+// Server-side Sentry is disabled for Netlify compatibility.
+// The @sentry/nuxt module with OpenTelemetry instrumentation has bundling
+// issues with Netlify's serverless functions.
+// Client-side error tracking remains fully functional via sentry.client.config.ts
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
+// If you need server-side error tracking on Netlify, consider:
+// 1. Using @sentry/node directly in API routes with try/catch
+// 2. Using Netlify's native Sentry integration
+// 3. Deploying to a platform with better OpenTelemetry support (Vercel, Railway)
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
-
-  // Enable sending of user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+export default {};
